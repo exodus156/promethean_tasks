f304c419c9fcf376d0fa3957eeb08db9ce9368ff
exodus156 <45183043+exodus156@users.noreply.github.com>
2024-03-23 15:26:34 +0100

    Feat(task_fe_i): Initialised task + added prettier + added scss (#1)

---
+++ Task I FE/.eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+	"plugin:import/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'import'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'import/newline-after-import': ['error', { count: 1 }],
+	'no-unused-vars': [
+		"error",
+		{
+		  "vars": "all",
+		  "args": "after-used",
+		  "ignoreRestSiblings": true,
+		  "argsIgnorePattern": "^_"
+		}
+	  ],
+	  'react/react-in-jsx-scope': "off"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    },
+    'import/resolver': {
+      node: {
+        paths: [
+          'src'
+        ],
+        extensions: [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
+  },
+};
